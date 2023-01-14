d72964343b4acb0738f01ba3219f040e66d8141b
Sean Marquez <capsulecorplab@gmail.com>
2023-01-13 17:21:14 -0700

    fix symlink error for Uploads/ in reset script

--- reset_kasm_user.sh
+++ reset_kasm_user.sh
@@ -4,3 +4,8 @@ mv ./kasm_user/_README ./_README
 rm -rf ./kasm_user/*
 rm -rf ./kasm_user/.*
 mv ./_README ./kasm_user/_README
+
+if [ ! -r kasm_user/Desktop/Uploads ]; then
+    mkdir -p kasm_user/Desktop/Uploads
+fi
+ln -srv ./kasm_user/Uploads ./kasm_user/Desktop/Uploads
