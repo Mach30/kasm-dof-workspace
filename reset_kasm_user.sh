733eac72dc1e785699a4e3b072a1902c885ffddc
Sean Marquez <capsulecorplab@gmail.com>
2023-03-25 09:28:34 -0700

    improve reset_kasm_user.sh
    replaces symlink command with `docker system prune` in reset_kasm_user.sh

--- reset_kasm_user.sh
+++ reset_kasm_user.sh
@@ -8,4 +8,5 @@ mv ./_README ./kasm_user/_README
 if [ ! -r kasm_user/Desktop/Uploads ]; then
     mkdir -p kasm_user/Desktop/Uploads
 fi
-ln -srv ./kasm_user/Uploads ./kasm_user/Desktop/Uploads
+
+docker system prune
